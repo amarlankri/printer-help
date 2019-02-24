c82fccd53c087e2ca6923b1631bdaa72f1cfb2bf
Amar Lankri <amar.lankri@gmail.com>
2019-02-24 19:28:01 +0100

    Change implementation of calculatePages

--- src/utils.js
+++ src/utils.js
@@ -6,23 +6,35 @@ export const calculatePages = (pages, pagePerPage) => {
         return null;
     }
 
-    let currentPage = 1;
-    let res = [];
+    const pageList = new Array(pagesInt)
+	  .fill(1)
+	  .map((e, i) => i + 1)
 
-    const gap = pagePerPageInt - 1;
-
-    while (currentPage + gap < pagesInt) {
-        res = res.concat(`${currentPage}-${currentPage + gap}`);
-        currentPage += pagePerPageInt;
+    const selectBorderPages = (acc, page) => {
+	const remainder = page % pagePerPageInt
+	return (remainder === 0 || remainder === 1 || page === pagesInt)
+	    ? [...acc, page]
+	    : acc
     }
+    const borderPages = pageList.reduce(selectBorderPages, [])
 
-    res = currentPage === pagesInt ? res.concat(pages) : res.concat(`${currentPage}-${pages}`);
-
-    const recto = res.filter((element, index) => !(index % 2));
-    const verso = res.filter((element, index) => index % 2);
-
-    return {
-        recto: recto.join(';'),
-        verso: verso.join(';')
+    const buildString = isRecto => (acc, page, index) => {
+	switch(index % 4) {
+	case 0:
+	    return isRecto ? acc + page : acc
+	case 1:
+	    return isRecto ? `${acc}-${page};` : acc
+	case 2:
+	    return isRecto ? acc : acc + page
+	case 3:
+   	    return isRecto ? acc : `${acc}-${page};`
+	default:
+	    return acc
+	}
     }
+    
+    const recto = borderPages.reduce(buildString(true), '')
+    const verso = borderPages.reduce(buildString(false), '')
+
+    return { recto, verso }
 }
